fe1ff5483125128da5446aa60bdaa3feda5508a4
Armen Tildian <tilarm@gmail.com>
2017-07-05 17:13:49 +0200

    * Card home page

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,16 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 
 class Home extends Component {
 
-    
-    render() {
-        return (
-            <div>
-              Home
-            </div>
-        );
-    }
+  play() {
+    // var audio = document.getElementById('audioElement');
+    // audio.play();
+  }
+
+  render() {
+    return (
+      <Card className="card">
+        <CardMedia
+          overlay={<CardTitle title="ROCK' N' ROLL" subtitle="78666 Abonnés"/>}
+        >
+          <img src="https://s3-us-west-2.amazonaws.com/teddarcuri.monarch/andrew+bird.jpg"/>
+        </CardMedia>
+        <CardTitle title="ROCK" subtitle=""/>
+        <CardText className="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+
+        <a onClick={this.play()} href="#">PLAY</a>
+
+        <CardActions>
+          <FlatButton label="PLAY"/>
+        </CardActions>
+
+      </Card>
+    );
+  }
 }
 
 export default Home;
